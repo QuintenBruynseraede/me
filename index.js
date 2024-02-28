7c619401314422e066617b31ec00d3f0246c6bf7
Quinten <quinten@dataroots.io>
2024-02-28 20:34:23 +0100

    Add link addon

--- index.js
+++ index.js
@@ -61,7 +61,10 @@ function handleKeyStroke(e) {
 
 function initializeTerminal() {
     t.open(document.getElementById('terminal'));
+    t.loadAddon(new WebLinksAddon.WebLinksAddon());
+    
     t.onKey(handleKeyStroke);
+   
     t.write(`Welcome to my interactive ${fmt.red('Terminal!')}\n\r`);
     t.write(`Use 'help' to see all available commands\n\r`); 
     t.write(fmt.newInputLine());
